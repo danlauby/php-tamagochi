dbe711ce58d3cc9599a9d651d691e7cc471a1eb2
Dan Lauby and Erica Wright <dan@>
2017-02-08 11:05:53 -0800

    update pet-care template with twig-in progress

--- app/app.php
+++ app/app.php
@@ -5,13 +5,29 @@
 
     $app = new Silex\Application();
 
-    $app->get("/my-pet", function() {
-        return "";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    session_start();
+    if (empty($_SESSION['pets'])) {
+        $_SESSION['pets'] = array();
+    }
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('pet-name.html.twig');
     });
 
-    $app->post("/view-pet", function() {
-        return
-        "";
+    $app->get("/pet-care", function() use ($app) {
+        $chaos = new Tamagochi("Chaos", 500, 800, 200);
+        $chaos->save();
+        $pets = array($chaos);
+        // return $chaos->getName();
+        return $app['twig']->render('pet-care.html.twig', array('my_pets' => Tamagochi::getAll()));
+    });
+
+    $app->get("/pet-view", function() use ($app) {
+        return $app['twig']->render('pet-view.html.twig');
     });
 
     return $app;
--- src/Tamagochi.php
+++ src/Tamagochi.php
@@ -1,15 +1,27 @@
 <?php
-    class Tamagotchi
+    class Tamagochi
     {
+        private $name;
         private $food;
         private $play;
         private $sleep;
 
-        function __construct($food, $play, $sleep)
+        function __construct($name, $food, $play, $sleep)
         {
+            $this->name = $name;
             $this->food = $food;
             $this->play = $play;
             $this->sleep = $sleep;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($newName)
+        {
+            $this->name = (string) $newName;
         }
 
         function getFood()
@@ -42,10 +54,14 @@
             $this->sleep = (string) $newSleep;
         }
 
-        // function save()
-        // {
-        //     array_push($_SESSION[])
-        // }
+        function save()
+        {
+            array_push($_SESSION['pets'], $this);
+        }
+        static function getAll()
+        {
+            return $_SESSION['pets'];
+        }
 
     }
 
