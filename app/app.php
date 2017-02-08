3094d9715f2374836eb22ae288e8c081cefe972a
Dan Lauby and Erica Wright <dan@>
2017-02-08 10:00:32 -0800

    create twig html templates

--- app/app.php
+++ app/app.php
@@ -1,11 +1,17 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Tamagochi.php";
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "Hello friend!";
+    $app->get("/my-pet", function() {
+        return "";
+    });
+
+    $app->post("/view-pet", function() {
+        return
+        "";
     });
 
     return $app;
